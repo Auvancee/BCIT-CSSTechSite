e42715ad046bd2cb9d644febcf23ae7388b9c080
EngEng-N <eeng3906@gmail.com>
2024-11-27 22:47:28 -0800

    Add html, css and js to transition webpage. box-hover-padding doesn't work.

---
+++ transition.js
@@ -0,0 +1,17 @@
+const divElem = document.getElementById("show");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", showHide);
+
+function showHide() {
+  divElem.classList.toggle("showing");
+}
+
+// const f = document.getElementById("foo");
+// document.addEventListener("click",
+//   (ev) => {
+//     f.style.transform = `translateY(${ev.clientY - 25}px)`;
+//     f.style.transform += `translateX(${ev.clientX - 25}px)`;
+//   },
+//   false,
+// );

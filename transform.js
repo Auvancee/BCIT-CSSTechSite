f2f18f0f4ef7d6739c6459d5218cb8c6b6480ce0
aleb88 <aswanson35@yahoo.com>
2024-12-02 01:18:58 -0800

    fix headings add more buttons improve layout

--- transform.js
+++ transform.js
@@ -1,8 +1,11 @@
 //buttons
 let transformBtn = document.getElementById("translate-button");
-let skewBtn = document.getElementById("skew-button");
+let skewXBtn = document.getElementById("skewX-button");
+let skewYBtn = document.getElementById("skewY-button");
+let skewXYBtn = document.getElementById("skewXY-button");
 let scaleBtn = document.getElementById("scale-button");
-let rotateBtn = document.getElementById("rotate-button");
+let rotateBtn1 = document.getElementById("rotate-button1");
+let rotateBtn2 = document.getElementById("rotate-button2");
 let matrixBtn = document.getElementById("matrix-button");
 //
 let redSquare1 = document.getElementById("red1"); 
@@ -14,16 +17,28 @@ transformBtn.addEventListener("click", function(){
     redSquare1.classList.toggle("translate");
 });
 
-skewBtn.addEventListener("click", function(){
-    redSquare2.classList.toggle("skew");
+skewXBtn.addEventListener("click", function(){
+    redSquare2.classList.toggle("skewX");
+});
+
+skewYBtn.addEventListener("click", function(){
+    redSquare2.classList.toggle("skewY");
+});
+
+skewXYBtn.addEventListener("click", function(){
+    redSquare2.classList.toggle("skewXY");
 });
 
 scaleBtn.addEventListener("click", function(){
     redSquare3.classList.toggle("scale");
 });
 
-rotateBtn.addEventListener("click", function(){
-    redSquare4.classList.toggle("rotate");
+rotateBtn1.addEventListener("click", function(){
+    redSquare4.classList.toggle("rotate1");
+});
+
+rotateBtn2.addEventListener("click", function(){
+    redSquare4.classList.toggle("rotate2");
 });
 
 matrixBtn.addEventListener("click", function(){
